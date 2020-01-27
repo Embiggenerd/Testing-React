99d2b1270cf688d9c829f0e4d8c6dadf6a5b30df
igor <embiggenerd@gmail.com>
2020-01-27 18:25:29 -0500

    tests for rendering a next button working

---
+++ star-wars-app/src/mockData/mockData.js
@@ -0,0 +1,10 @@
+export const mockData = {
+    previus: null,
+    next: "nextURL",
+    results: [
+        {name: "luke", url:"luke"},
+        {name: "luke1", url:"luke1"},
+        {name: "luke2", url:"luke2"},
+        {name: "luke4", url:"luke4"},
+    ]
+}
